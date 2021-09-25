72575add02f21f8e7488059c7c33a2d86997c29a
Yerra Akash <91045269+akashyerra@users.noreply.github.com>
2021-09-25 11:10:13 +0530

    Add files via upload

---
+++ Printing numbers from 1 to 10.c
@@ -0,0 +1,11 @@
+/*Printing numbers from 1 to 10*/
+#include<stdio.h>
+void main()
+{
+	int number;//Initialization
+	while(number <= 10)//looping and printing
+	{
+		printf("%d ", number);
+		number++;
+	}
+}
